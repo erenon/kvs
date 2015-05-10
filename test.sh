5d271f063c9d02c5385ff534ff9d126c66080a36
erenon <erenon2@gmail.com>
2015-05-10 17:02:50 +0200

    test.sh: exclude .so files

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ bin_dir="$( cd "$( dirname "$0" )" && pwd )"
 CXX=${CXX:="g++"}
 test_dir="$bin_dir/build/$CXX/release/test"
 
-find $test_dir -type f -print0 | while IFS= read -r -d $'\0' testprog; do
+find $test_dir -type f -not -name "*.so" -print0 | while IFS= read -r -d $'\0' testprog; do
+  echo "run $testprog"
   $testprog
 done
